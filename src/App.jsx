de99a8cea9a59849b7aa8a12c980364b764ccf1e
Afaf KELAI <ea_kelai@esi.dz>
2021-12-29 17:22:32 +0100

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { useState } from 'react'
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [v, setv] = useState("")
+  const getValue = (e) => {
+    setInputValue(e.target.value)
+  }
+  const printValue = (e) => {
+    e.preventDefault()
+    setv(inputValue)
+  }
+
   return (
     <div>
-      
+      <form onSubmit={printValue} action="">
+        <input onChange={getValue} type="text" />
+        <button type='submit' >Submit</button>
+      </form>
+
+
+      <h1>input Value : {v} </h1>
     </div>
   )
 }
